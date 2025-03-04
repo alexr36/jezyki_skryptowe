c04a9aeff651f3968cc6d74fb2530f93b1381918
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-03-04 21:45:07 +0100

    Add files via upload

---
+++ Lab1/zad4.py
@@ -0,0 +1,4 @@
+a = int(input("Enter base length: "))
+h = int(input("Enter height: "))
+
+print(a * h / 2)
---
+++ Lab1/zad5.py
@@ -0,0 +1,4 @@
+import pyfiglet
+
+text = input("Enter text: ")
+pyfiglet.print_figlet(text)
