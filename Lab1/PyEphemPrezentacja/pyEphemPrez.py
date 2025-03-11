af21b7824977b72d5fe356fd02520ee912334dcf
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-03-11 16:56:25 +0100

    Update pyEphemPrez.py

--- Lab1/PyEphemPrezentacja/pyEphemPrez.py
+++ Lab1/PyEphemPrezentacja/pyEphemPrez.py
@@ -15,28 +15,24 @@ print(f"Zachod Slonca: {sunset}\n")
 
 
 #   Obliczanie aktualnej pozycji Słońca
-
 sun = ephem.Sun()
 sun.compute()
 print(f"Slonce - RA: {sun.ra}, DEC: {sun.dec}\n")
 
 
 #   Obliczanie fazy Księżyca dla zadanej daty
-
 moon = ephem.Moon()
 moon.compute('2025/03/13')
 print(f"Faza Ksiezyca: {moon.phase}\n")
 
 
 #   Obliczanie pozycji planet - Mars
-
 mars = ephem.Mars()
 mars.compute()
 print(f"Mars - RA: {mars.ra}, DEC: {mars.dec}\n")
 
 
 #   Obliczanie dla własnych obiektów astronomicznych
-
 my_star = ephem.readdb("MyStar,f|M|F7,10:08:22.3,+11:58:02,2.0,2000")
 my_star.compute()
 print(f"Moja gwiazda - RA: {my_star.ra}, DEC - {my_star.dec}\n")
