13d8bc9622b91b7ac7f84509050a1c0d39eeb0e8
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-04-18 01:05:14 +0200

    Update 4th_quartile_sentences.py

--- Lab2/filters/4th_quartile_sentences.py
+++ Lab2/filters/4th_quartile_sentences.py
@@ -1,29 +1,29 @@
 from aux.aux_methods import extractSentences, countAlphanumerals
 
 
-def find4thQuartileSentences():
+def find_4th_quartile_sentences():
     result_sentences = ''
-    sentences = list(extractSentences())
+    sentences = list(extract_sentences())
     
     if not sentences:
         return result_sentences
 
-    sentences_lengths = sorted(countAlphanumerals(sentence.strip()) for sentence in sentences)
+    sentences_lengths = sorted(count_alphanumerals(sentence.strip()) for sentence in sentences)
     sentences_count = len(sentences_lengths)
 
     q3_index = int(round(0.75 * sentences_count)) - 1
     q3_value = sentences_lengths[max(q3_index, 0)]
 
     for sentence in sentences:
-        if countAlphanumerals(sentence) > q3_value:
+        if count_alphanumerals(sentence) > q3_value:
             result_sentences += sentence + '\n'
 
     return result_sentences
 
 
-def print4thQuartileSentences():
-    print(find4thQuartileSentences())
+def print_4th_quartile_sentences():
+    print(find_4th_quartile_sentences())
 
 
 if __name__ == '__main__':
-    print4thQuartileSentences()        
+    print_4th_quartile_sentences()        
