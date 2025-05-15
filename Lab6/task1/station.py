870c43c303f7fdb188ff812217576499ec4f615b
Alex Rogoziński <39859634+alexr36@users.noreply.github.com>
2025-05-15 20:21:42 +0200

    Update station.py

--- Lab6/task1/station.py
+++ Lab6/task1/station.py
@@ -2,27 +2,24 @@ class Station:
     '''Class representing station's data based on 'stacje.csv' file.'''
 
     def __init__(
-            self, code, intern_code,
-            name, old_code, launch, 
-            close, type, area, kind, 
-            voiv, city, addr, 
-            lat, lon
+            self, code, intern_code, name, old_code, 
+            launch, close, st_type, area, kind, 
+            voiv, city, addr, lat, lon
         ):
-        self.code = code
+        self.code        = code
         self.intern_code = intern_code
-        self.name = name
-        self.old_code = old_code
-        self.launch = launch
-        self.close = close
-        self.type = type
-        self.area = area
-        self.kind = kind
-        self.voiv = voiv
-        self.city = city
-        self.addr = addr
-        self.lat = lat
-        self.lon = lon
-
+        self.name        = name
+        self.old_code    = old_code
+        self.launch      = launch
+        self.close       = close
+        self.st_type     = st_type
+        self.area        = area
+        self.kind        = kind
+        self.voiv        = voiv
+        self.city        = city
+        self.addr        = addr
+        self.lat         = lat
+        self.lon         = lon
 
 
     def __str__(self):
@@ -33,7 +30,7 @@ class Station:
             f"'old_code':           {self.old_code}\n"
             f"'launch_date':        {self.launch}\n"
             f"'close_date':         {self.close}\n"
-            f"'station_type':       {self.type}\n"
+            f"'station_type':       {self.st_type}\n"
             f"'area_type':          {self.area}\n"
             f"'station_kind':       {self.kind}\n"
             f"'voivodeship':        {self.voiv}\n"
@@ -42,7 +39,6 @@ class Station:
             f"'latitude':           {self.lat}\n"
             f"'longitude':          {self.lon})"
         )
-
 
 
     def __repr__(self):
@@ -54,7 +50,7 @@ class Station:
             f"old_code=             {self.old_code!r}, "
             f"launch_date=          {self.launch!r}, "
             f"close_date=           {self.close!r}, "
-            f"station_type=         {self.type!r}, "
+            f"station_type=         {self.st_type!r}, "
             f"area_type=            {self.area!r}, "
             f"station_kind=         {self.kind!r}, "
             f"voivodeship=          {self.voiv!r}, "
@@ -63,7 +59,6 @@ class Station:
             f"latitude=             {self.lat!r}, "
             f"longitude=            {self.lon!r}"
         )
-
 
 
     def __eq__(self, other):
